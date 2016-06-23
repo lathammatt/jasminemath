a795d2bd59404c348312f22bb359f8f974f3e57b
Matt Latham <latham75@gmail.com>
2016-06-23 11:52:07 -0500

    math functions complete

---
+++ src/Math.js
@@ -0,0 +1,23 @@
+function add (num1, num2){
+	return num1 + num2;
+};
+
+function subtract (num1, num2) {
+	return num1 - num2;
+};
+
+function multiply (num1, num2){
+	return num1 * num2;
+};
+
+function divide (num1, num2){
+	return num1 / num2;
+};
+
+function square (num1){
+	return num1 * num1;
+};
+
+function sroot (num1){
+	return Math.sqrt(num1);
+};
