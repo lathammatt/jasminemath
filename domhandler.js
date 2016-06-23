68e0aa6c432138c3c4bbd7ddf998dbb5e2463aad
Matt Latham <latham75@gmail.com>
2016-06-23 14:12:07 -0500

    finished unstyled calculator

---
+++ domhandler.js
@@ -0,0 +1,7 @@
+var input = document.addEventHandler("input");
+var add = document.addEventHandler("add");
+var minus = document.addEventHandler("minus");
+var times = document.addEventHandler("times");
+var divide = document.addEventHandler("divide");
+var square = document.addEventHandler("square");
+var rooty = document.addEventHandler("root");
